faf679f53a72c51b0283c61861cf2616322b2fc8
sofalotfy <lotfysofa@gmail.com>
2026-07-07 00:01:57 +0300

    Downgrade to Laravel 12

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,29 +13,29 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('image')->nullable();
+            $table->string('name', 191);
+            $table->string('image', 191)->nullable();
             $table->string('email', 191)->unique();
-            $table->string('phone');
+            $table->string('phone', 191);
             $table->integer('age');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('gender')->default('male');
+            $table->string('gender', 191)->default('male');
             $table->text('address');
-            $table->string('password');
+            $table->string('password', 191);
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
             $table->string('email', 191)->primary();
-            $table->string('token');
+            $table->string('token', 191);
             $table->timestamp('created_at')->nullable();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
             $table->string('id', 191)->primary();
             $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
+            $table->string('ip_address', 191, 45)->nullable();
             $table->text('user_agent')->nullable();
             $table->longText('payload');
             $table->integer('last_activity')->index();
