da0c50f6b0215ebe81619491c8eae058a5eb6996
sofalotfy <lotfysofa@gmail.com>
2026-07-06 22:57:24 +0300

    doeO

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -4,9 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Enums\AppointmentStatus;
+use App\Enums\AppointmentGrade;
+
 class Appointment extends Model
 {
     protected $fillable = ['doctor_id', 'patient_id', 'status', 'date', 'delay', 'duration', 'grade'];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'status' => AppointmentStatus::class,
+            'grade' => AppointmentGrade::class,
+        ];
+    }
 
     /**
      * Get the doctor for this appointment.
