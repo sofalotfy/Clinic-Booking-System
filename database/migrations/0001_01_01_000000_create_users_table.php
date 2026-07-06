2d7e28584df0282ba078a704295d9ed060e93b2e
sofalotfy <lotfysofa@gmail.com>
2026-07-06 23:46:03 +0300

    Downgrade to Laravel 12

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('image')->nullable();
-            $table->string('email')->unique();
+            $table->string('email', 191)->unique();
             $table->string('phone');
             $table->integer('age');
             $table->timestamp('email_verified_at')->nullable();
