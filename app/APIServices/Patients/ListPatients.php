8f528b02182709e590b0ae07f49e7997c31604e7
sofalotfy <lotfysofa@gmail.com>
2026-07-07 15:55:31 +0300

    sa

--- app/APIServices/Patients/ListPatients.php
+++ app/APIServices/Patients/ListPatients.php
@@ -6,6 +6,7 @@ use App\Models\Patient;
 use App\Models\User;
 use App\Models\Appointment;
 use App\Enums\UserType;
+use Illuminate\Support\Facades\DB;
 
 
 class ListPatients
@@ -43,6 +44,8 @@ class ListPatients
             'users.image as avatar',
             'users.age as age',
             'users.address as address',
+            'appointment_summary.last_appointment',
+            'appointment_summary.upcoming_appointment',
         ];
     }
 
@@ -51,13 +54,32 @@ class ListPatients
         $user = User::where('id', $userId)->first();
 
         if($user->type == UserType::DOCTOR){
+            $appointmentSummary = self::getAppintmentSummarData($user->doctor->id);
+
             return Patient::leftJoin('appointments', 'patients.id', '=', 'appointments.patient_id')
                         ->leftJoin('users', 'patients.user_id', '=', 'users.id')
+                        ->leftJoinSub($appointmentSummary, 'appointment_summary', function ($join) {
+                            $join->on('patients.id', '=', 'appointment_summary.patient_id');
+                        })
                         ->where('appointments.doctor_id', $user->doctor->id);
         }
         else{
             return null;
         }
+    }
+
+
+    private static function getAppintmentSummarData(int $doctorId)
+    {
+        $appointmentSummary = Appointment::select(
+            'patient_id',
+            DB::raw('MAX(CASE WHEN date < NOW() THEN date END) as last_appointment'),
+            DB::raw('MIN(CASE WHEN date >= NOW() THEN date END) as upcoming_appointment')
+        )
+        ->where('doctor_id', $doctorId)
+        ->groupBy('patient_id');
+
+        return $appointmentSummary;
     }
 
 }
