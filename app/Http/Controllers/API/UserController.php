d55169843e040ddc65fde542ef16aa2d39089c6f
sofalotfy <lotfysofa@gmail.com>
2026-07-07 12:15:57 +0300

    s

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\APIServices\Users\GetUser;
+use App\Models\User;
 
 class UserController extends Controller
 {
