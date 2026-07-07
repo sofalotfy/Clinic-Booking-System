fbe60c46e03ab5fcb0973a44ff1765d42b495177
sofalotfy <lotfysofa@gmail.com>
2026-07-07 12:55:34 +0300

    sa

--- routes/api.php
+++ routes/api.php
@@ -12,12 +12,13 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/logout', [UserController::class, 'logout']);
     Route::get('/me', [UserController::class, 'me']);
+    Route::post('/edit-profile', [UserController::class, 'editProfile']);
 
-    Route::get('/tasks', [TaskController::class, 'index']);      // list
-    Route::post('/tasks', [TaskController::class, 'store']);     // create
-    Route::get('/tasks/form', [TaskController::class, 'form']); // form
-    Route::put('/tasks/{task}', [TaskController::class, 'update']); // update
-    Route::delete('/tasks/{task}', [TaskController::class, 'destroy']); // delete
+    // Route::get('/tasks', [TaskController::class, 'index']);      // list
+    // Route::post('/tasks', [TaskController::class, 'store']);     // create
+    // Route::get('/tasks/form', [TaskController::class, 'form']); // form
+    // Route::put('/tasks/{task}', [TaskController::class, 'update']); // update
+    // Route::delete('/tasks/{task}', [TaskController::class, 'destroy']); // delete
 
 });
 
