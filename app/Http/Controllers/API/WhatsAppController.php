285d4393f38a18a72e826ba48a1d297981caeaf9
sofalotfy <lotfysofa@gmail.com>
2026-07-08 19:10:53 +0300

    as

--- app/Http/Controllers/API/WhatsAppController.php
+++ app/Http/Controllers/API/WhatsAppController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use App\Models\Doctor;
 use App\APIServices\WhatsApp\SendMessage;
+use App\Models\DoctorWhatsAppAccount;
 
 class WhatsAppController extends Controller
 {
