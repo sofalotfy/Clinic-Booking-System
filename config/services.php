52a5dc366a02eaba3295ed674922ad3c4fa0075c
sofalotfy <lotfysofa@gmail.com>
2026-07-08 15:07:11 +0300

    as

--- config/services.php
+++ config/services.php
@@ -34,5 +34,12 @@ return [
             'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
         ],
     ],
+    
+    'whatsapp' => [
+        'token' => env('WHATSAPP_TOKEN'),
+        'phone_number_id' => env('WHATSAPP_PHONE_NUMBER_ID'),
+        'verify_token' => env('WHATSAPP_VERIFY_TOKEN'),
+        'version' => env('WHATSAPP_GRAPH_VERSION', 'v23.0'),
+    ],
 
 ];
