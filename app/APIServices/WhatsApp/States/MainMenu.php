e71ff00b41bbe215137dcc4befe7d17453f60ba6
sofalotfy <lotfysofa@gmail.com>
2026-07-14 15:14:07 +0300

    asa

--- app/APIServices/WhatsApp/States/MainMenu.php
+++ app/APIServices/WhatsApp/States/MainMenu.php
@@ -123,10 +123,6 @@ class MainMenu
                 break;
 
             case 'book_appointment':
-                $conversation->update([
-                    'state' => ConversationState::BOOK_APPOINTMENT,
-                ]);
-
                 return BookAppointment::execute($conversation, $message);
                 break;
 
