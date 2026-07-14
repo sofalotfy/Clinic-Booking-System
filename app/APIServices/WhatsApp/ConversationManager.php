d92ddf89c80f8f9d5ae2af94b0c52306deaabae1
sofalotfy <lotfysofa@gmail.com>
2026-07-14 14:45:04 +0300

    asa

--- app/APIServices/WhatsApp/ConversationManager.php
+++ app/APIServices/WhatsApp/ConversationManager.php
@@ -13,6 +13,14 @@ class ConversationManager
 {
     public static function execute(array $payload)
     {
+        \Log::info(json_encode($payload, JSON_PRETTY_PRINT));
+        $value = $payload['entry'][0]['changes'][0]['value'] ?? [];
+
+        if (! isset($value['messages'][0])) {
+            // Ignore status updates and other webhook events
+            return;
+        }
+
         // 1. Extract the message from the webhook
         $message = self::extractMessage($payload);
 
