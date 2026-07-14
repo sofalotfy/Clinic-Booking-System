c6f333ce04aa3ea09bdc005856b8d62754f4828f
sofalotfy <lotfysofa@gmail.com>
2026-07-14 15:41:49 +0300

    asa

--- app/APIServices/WhatsApp/ConversationRouter.php
+++ app/APIServices/WhatsApp/ConversationRouter.php
@@ -6,9 +6,10 @@ use App\Enums\ConversationState;
 use App\Models\WhatsAppConversation;
 use App\APIServices\WhatsApp\States\Start;
 use App\APIServices\WhatsApp\States\MainMenu;
-use App\APIServices\WhatsApp\States\BookingState;
-use App\APIServices\WhatsApp\States\RescheduleState;
 use App\APIServices\WhatsApp\States\CancelState;
+use App\APIServices\WhatsApp\States\InfoInquiry;
+use App\APIServices\WhatsApp\States\InfoConfirmation;
+use App\APIServices\WhatsApp\States\BookAppointment;
 
 class ConversationRouter
 {
@@ -21,8 +22,14 @@ class ConversationRouter
             ConversationState::MAIN_MENU =>
                 MainMenu::handleResponse($conversation, $message),
 
-            // ConversationState::BOOKING =>
-            //     BookingState::execute($conversation, $message),
+            ConversationState::BOOK_APPOINTMENT =>
+                BookAppointment::handleResponse($conversation, $message),
+
+            ConversationState::INFO_INQUIRY =>
+                InfoInquiry::handleResponse($conversation, $message),
+
+            ConversationState::INFO_CONFIRMATION =>
+                InfoConfirmation::handleResponse($conversation, $message),
 
             // ConversationState::RESCHEDULE =>
             //     RescheduleState::execute($conversation, $message),
