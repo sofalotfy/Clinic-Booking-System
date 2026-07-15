d394dd0500c6eee7e2c86c9f836a51a80de5a671
sofalotfy <lotfysofa@gmail.com>
2026-07-15 18:02:05 +0300

    Asa

--- app/APIServices/Notes/EditNote.php
+++ app/APIServices/Notes/EditNote.php
@@ -8,7 +8,6 @@ class EditNote
 {
     public static function execute($noteId,  $note)
     {
-        $note = Note::find($noteId);
         // if ($note->doctor_id !== auth()->user()->doctor->id) {
         //     throw new \Exception('You are not authorized to edit this note');
         // }
