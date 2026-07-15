13346429edd0947f54c8737a1986971535e977cc
sofalotfy <lotfysofa@gmail.com>
2026-07-15 18:10:09 +0300

    Asa

--- app/APIServices/Notes/EditNote.php
+++ app/APIServices/Notes/EditNote.php
@@ -10,7 +10,7 @@ class EditNote
     {
         $note = Note::find($noteId);
 
-        if ($note->doctor_id !== auth()->user()->doctor->id) {
+        if ($note->doctor_id != auth()->user()->doctor->id) {
             throw new \Exception("You are not authorized to edit this note my id {$note->doctor_id} doctor id " . auth()->user()->doctor->id);
         }
 
