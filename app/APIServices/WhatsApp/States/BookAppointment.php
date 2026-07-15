f69c8a6a97c74e3a9af2a09ff6bb119d245e1339
sofalotfy <lotfysofa@gmail.com>
2026-07-15 12:44:06 +0300

    as

--- app/APIServices/WhatsApp/States/BookAppointment.php
+++ app/APIServices/WhatsApp/States/BookAppointment.php
@@ -9,7 +9,7 @@ use App\Services\Appointments\GetUpComingAppointment;
 use App\APIServices\Doctors\GetAvailableDays;
 use App\APIServices\WhatsApp\States\BookSlot;
 use App\Models\Day;
-use App\APIService\Day\CheckAvailability;
+use App\APIService\Days\CheckAvailability;
 
 
 class BookAppointment
