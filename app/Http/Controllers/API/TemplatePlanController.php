73f0fe730fa6c873e52c1c577dc8fea81e5f09fb
sofalotfy <lotfysofa@gmail.com>
2026-07-15 19:22:19 +0300

    Asa

--- app/Http/Controllers/API/TemplatePlanController.php
+++ app/Http/Controllers/API/TemplatePlanController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Services\TemplatePlans\CreteaTemplate;
 use App\Services\TemplatePlans\UpdatePlan;
+use App\Services\TemplatePlans\DeletePlan;
 use App\Models\TemplatePlan;
 use App\Enums\TemplatePlanStatus;
 
