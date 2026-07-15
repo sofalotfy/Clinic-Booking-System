99e61b44b447eb672b0778765cc74ab8cc22f805
sofalotfy <lotfysofa@gmail.com>
2026-07-15 19:04:06 +0300

    Asa

--- app/Services/TemplatePlans/UpdatePlan.php
+++ app/Services/TemplatePlans/UpdatePlan.php
@@ -3,23 +3,31 @@
 namespace App\Services\TemplatePlans;
 
 use App\Models\TemplatePlan;
-use App\Services\TemplateDays\CreateTemplateDay;
+use App\Services\TemplateDays\UpdateTemplateDay;
 
 class UpdatePlan
 {
     public static function execute($name, $description, $days, $plan_id)
     {
-        $template = TemplatePLan::find($plan_id);
-        
-        $template::update([
-            'name'      => $name,
-            'description' => $description,
+        $template = TemplatePlan::findOrFail($plan_id);
+
+        $template->update([
+            'name' => $name ?? $template->name,
+            'description' => $description ?? $template->description,
         ]);
 
-        foreach ($template->templateDays as $index => $templateDay) {
-            UpdateTemplateDay::execute($templateDay, $days[$index]);
+        if ($days) {
+            $requestDays = collect($days)->keyBy('day_of_week');
+
+            foreach ($template->templateDays as $templateDay) {
+                $day = $requestDays->get($templateDay->day_of_week);
+
+                if ($day) {
+                    UpdateTemplateDay::execute($templateDay, $day);
+                }
+            }
         }
 
-        return $template->with('templateDays');
+        return $template->load('templateDays');
     }
 }
