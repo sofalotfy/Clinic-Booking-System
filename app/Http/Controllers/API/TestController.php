6c80a2420f4e786d327e8c7145337ed885ac922d
sofalotfy <lotfysofa@gmail.com>
2026-07-20 15:43:28 +0300

    as

--- app/Http/Controllers/API/TestController.php
+++ app/Http/Controllers/API/TestController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Services\TemplatePlans\CreteaTemplate;
 use App\APIServices\Days\GetAvailableSlots;
 use App\APIServices\Doctors\GetAvailableDays;
+USE App\Services\TemplatePlans\ActivatePlan;
 
 class TestController extends Controller
 {
