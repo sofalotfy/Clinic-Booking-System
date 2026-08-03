66b19e0ba7ccb25af1e25457fcb6581e2ea9b71c
sofalotfy <lotfysofa@gmail.com>
2026-08-03 15:18:33 +0300

    as:

--- app/APIServices/Patients/GetPatient.php
+++ app/APIServices/Patients/GetPatient.php
@@ -11,8 +11,11 @@ use Illuminate\Support\Facades\DB;
 
 class GetPatient
 {
-    public static function execute(int $userId, int $patientId)
+    public static function execute(int $userId = null, int $patientId)
     {
+        if(!$userId){
+            $userId = auth()->user()->id;
+        }
         $query = self::getAuthData($userId);
 
         if (!$query) {
