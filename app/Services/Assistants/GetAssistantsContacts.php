a73133632fbff35d85cacde1b3224f8d1c1bc184
sofalotfy <lotfysofa@gmail.com>
2026-08-10 21:30:12 +0300

    As

--- app/Services/Assistants/GetAssistantsContacts.php
+++ app/Services/Assistants/GetAssistantsContacts.php
@@ -12,6 +12,11 @@ class GetAssistantsContacts
         $assistants = Assistant::where('doctor_id', $doctor->id)->get();
         $contacts = [];
         foreach ($assistants as $assistant) {
+            return response()->json([
+            'success' => true,
+            'assistant_id' => $assistant->id,
+            'user_id' => $assistant->user_id,
+        ]);
             $contacts[] = [
                 'name' => $assistant->user->name,
                 'phone' => $assistant->user->phone,
