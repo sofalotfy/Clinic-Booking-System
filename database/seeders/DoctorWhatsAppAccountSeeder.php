380e3796e550505e4f082e0d55959462839a4c0d
sofalotfy <lotfysofa@gmail.com>
2026-08-11 12:22:25 +0300

    as

--- database/seeders/DoctorWhatsAppAccountSeeder.php
+++ database/seeders/DoctorWhatsAppAccountSeeder.php
@@ -16,7 +16,7 @@ class DoctorWhatsAppAccountSeeder extends Seeder
             ['doctor_id' => 1],
             [
                 'phone_number_id' => '1137805152755860',
-                'access_token' => 'EABBbX1ZCpBBsBSPUp8larUeWbROGmgn2eGdWZABAhjrRAIW6Pn4c9HF48dtmwoqSu3QweFAgs8wDwC9ZCRwHg1GOkKZAu1lDtaE8lvgIOgXm9xQUtSWw2SUjh3OC3rLiRlXUc5mIjMAY6t9eGCMvuitAt0kjdElfZCh7OfdsVyNCkKTNKJBJgtkc9P4X9mlZCDaAsfp4jHrst0Luw07jjoAIDlFj9LPKDuU9TRZBZC9QeA0Obbi5UPJfG6lCKfd2anFjzPa8E0lvQkZBUgAGafTu4gQyO',
+                'access_token' => 'EABBbX1ZCpBBsBSKF07AAZBOn2b77BzyiDAPIgr7PBxQc5rwqtii3xNdYTJcObhNylJ5Tkj1qa2L1SSiRgSDLBV9lTDdOIA6jWnstsisTTWHi12zMya5w3nGAYBE38WhqVQf650A0g7G7VBmXGKwiyQk5xKm1rWwv3z5VZAGh6snZBj5Df608YGoZCqZASmw0FVCpVW6pS6Q5K8JiE1xEJrq08I9buP0qhu4sa188BOOawKHCVomRFQPykVn2phr96oylEpVLAzSE99LV3XwPnpcAVTxAZDZD',
                 'is_active' => true,
                 //commit
             ]
