9034befedff3665ac7593d02fe1eee80971224cc
sofalotfy <lotfysofa@gmail.com>
2026-08-11 18:59:35 +0300

    as

--- database/seeders/DoctorWhatsAppAccountSeeder.php
+++ database/seeders/DoctorWhatsAppAccountSeeder.php
@@ -18,7 +18,6 @@ class DoctorWhatsAppAccountSeeder extends Seeder
                 'phone_number_id' => '1137805152755860',
                 'access_token' => 'EABBbX1ZCpBBsBSDgKjvGxpKMnTWbS3FskWaqc7Vyi9c5orFyC78ZCJidgz706xauPjBaUrt1ov7hjDuJhB2k4pvnZAItaMhWfRWGZAnwayjuWxQMQZAQlCKbZAPbETEQlUDgM4iKUJCNCOtBU33bg1FdZAEOl9doQltXRLPDQo3YVLlZC6739OT9ZC5D9dklNpaZBcDVBepZAjkhF4YYI7oZAerIbGvuQEt6VSYCJO56ZCUhtmRJ3xUwtjLuraVmGmLtK33aiTAwSBLDz9hK1RhGvwawOWWVK',
                 'is_active' => true,
-                //commit
             ]
         );
     }
