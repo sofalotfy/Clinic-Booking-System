1368a2cce6eba4351a8fed108e8edbbc93347b1c
sofalotfy <lotfysofa@gmail.com>
2026-09-05 14:27:06 +0300

    migration fix

--- database/migrations/2026_09_04_132433_update_notifications_table.php
+++ database/migrations/2026_09_04_132433_update_notifications_table.php
@@ -2,14 +2,28 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
     public function up(): void
     {
+        $foreignKeyExists = DB::select("
+            SELECT CONSTRAINT_NAME FROM information_schema.TABLE_CONSTRAINTS
+            WHERE TABLE_SCHEMA = DATABASE()
+              AND TABLE_NAME = 'notifications'
+              AND CONSTRAINT_TYPE = 'FOREIGN KEY'
+              AND CONSTRAINT_NAME = 'notifications_user_id_foreign'
+        ");
+
+        if (count($foreignKeyExists) > 0) {
+            Schema::table('notifications', function (Blueprint $table) {
+                $table->dropForeign(['user_id']);
+            });
+        }
+
         Schema::table('notifications', function (Blueprint $table) {
-            $table->dropForeign(['user_id']);
             $table->dropColumn(['type', 'user_id']);
 
             $table->foreignId('sender_id')->nullable()->constrained('users')->nullOnDelete();
