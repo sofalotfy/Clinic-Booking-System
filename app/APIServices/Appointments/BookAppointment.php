77f5fb80dfbf6027fcca30da30927a86f3c4bcab
sofalotfy <lotfysofa@gmail.com>
2026-09-09 17:58:45 +0300

    added permissions

--- app/APIServices/Appointments/BookAppointment.php
+++ app/APIServices/Appointments/BookAppointment.php
@@ -28,7 +28,7 @@ class BookAppointment
         $dateTime = Carbon::parse($validated['date']);
 
         //GET BOOKING DAY INSTANCE
-        $day = Day::where('doctor_id', $request->user()->clinicDoctorId)
+        $day = Day::where('doctor_id', $request->user()->clinicDoctorId())
             ->whereDate('date', $dateTime->toDateString())
             ->first();
 
