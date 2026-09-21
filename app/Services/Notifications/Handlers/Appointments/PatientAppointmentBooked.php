2192fc9cb16bf914fce94a9235ae6800aeade2ff
sofalotfy <lotfysofa@gmail.com>
2026-09-21 17:39:31 +0300

    use first template in doctor appointment creation

--- app/Services/Notifications/Handlers/Appointments/PatientAppointmentBooked.php
+++ app/Services/Notifications/Handlers/Appointments/PatientAppointmentBooked.php
@@ -17,7 +17,7 @@ class PatientAppointmentBooked extends Handler
         $title = static::buildTitle($model, $notification);
         $body = static::buildBody($model, $notification);
 
-        static::dispatch($sender, $clinicId, $notification, $model, $receivers, $title, $body);
+        static::dispatch($sender, $clinicId, $notification, $receivers, $model, $title, $body);
     }
 
     private static function buildTitle(Model $model, $notification): string
